20f48e2b69b742b7ccbc64f3a17a75e4e02a4fd7
blakejmcguire <blake@blakemcguire.com>
2025-01-01 14:32:34 -0800

    Set up pug

--- app.cjs
+++ app.cjs
@@ -1,8 +1,18 @@
+// Configure express.js
+
 const express = require('express')
 const app = express()
+app.set('view engine', 'pug')
+app.use(express.json())
+
+// Set up public folder
+
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.render('index')
 })
 
 module.exports = app
