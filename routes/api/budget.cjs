d782f432990a042903f24ce72f23303ede4cb3d2
blakejmcguire <blake@blakemcguire.com>
2025-07-31 20:39:58 -0700

    Implemented user object in separate route.

--- routes/api/budget.cjs
+++ routes/api/budget.cjs
@@ -5,34 +5,37 @@ const budgetApi = new Router()
 
 const User = require('../../classes/User.cjs')
 
-budgetApi.get('/', (req, res) => {
-    let user = new User(req.query.userId)
-    let budgetItems = user.budget.getItems()
+budgetApi.post('*', (req, res, next) => {
+    req.user = new User(req.body.userId)
+    next()
+})
+
+budgetApi.post('/', (req, res) => {
+    let budgetItems = req.user.budget.getItems()
     res.send(budgetItems)
 })
 
-budgetApi.get('/all', (req, res) => {
-    //console.log(req.body)
-})
-
 budgetApi.post('/add', (req, res) => {
-    let user = new User(req.query.userId)
     let newItem = {
         name: req.body.name,
         amount: req.body.amount,
         date: req.body.date,
         paymentsPerYear: req.body.paymentsPerYear
     }
-    let newId = user.budget.addItem(newItem)
-    user.save()
+    let newId = req.user.budget.addItem(newItem)
+    req.user.save()
     res.send(newId)
+})
+
+budgetApi.post('/edit', (req, res) => {
+    let itemId = req.body.id
+    let user = new User(req.body.userId) 
 })
 
 budgetApi.post('/delete', (req, res) => {
     let itemId = req.body.id
-    let user = new User(req.query.userId)
-    user.budget.deleteItem(itemId)
-    user.save()
+    req.user.budget.deleteItem(itemId)
+    req.user.save()
     res.sendStatus(200)
 })
 
