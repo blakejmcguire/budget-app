d9abd8dc069bf366794adc5d914509c808e0eb8f
Blake McGuire <blake@blakemcguire.com>
2025-08-02 19:10:12 -0700

    Made cashflow ui more dynamic.

--- public/cashflow.js
+++ public/cashflow.js
@@ -1,38 +1,50 @@
 $(async () => {
-    // Set input fields to today and 2 weeks from today
+    $("#from").val('2025-01-01')
+    $("#to").val('2025-06-30')
+
+    // Initial build of the data table
+    update()
+
+    $("#from").on("change", update);
+    $("#to").on("change", update);
+})
+
+function setDates() {
     let d = new Date()
     let today = `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')}`
     d.setDate(d.getDate() + 14)
     let later = `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')}`
     $("#from").val(today)
     $("#to").val(later)
-
-    // Initial build of the data table
-    makeTable(await getData());
-
-    $("#from").on("change", update);
-    $("#to").on("change", update);
-
-    await getData()
-})
+}
 
 async function update() {
-    t = document.querySelector("table");
-    if (t) 
-        // Remove the existing table if it exists
-        t.remove();
-    makeTable(await getData())
+    let data = await getData()
+    makeTable(data)
 }
 
 async function getData() {
     let from = $("#from").val()
     let to = $("#to").val()
-    let res = await fetch(`/api/schedule?from=${from}&to=${to}`)
+
+    let res = await fetch(`/api/cashflow/getTemp?from=${from}&to=${to}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "userId": "test"
+        })
+    })
     let data = await res.json()
+
     return data
 }
 
 function makeTable(data) {
+    console.log(data)
+    $('#cashflow-table').remove()
+
     let $table = $('<table>')
     $table.attr('id', 'cashflow-table')
     $table.addClass("table")
@@ -42,8 +54,8 @@ function makeTable(data) {
 
     let total = 0;
     data.forEach(datum => {
-        $table.children('tbody').append(`<tr><td>${datum.date}</td><td>${datum.name}</td><td>$${datum.amount.toFixed(2)}</td></tr>`)
-        total += datum.amount
+        $table.children('tbody').append(`<tr><td>${datum.date}</td><td>${datum.name}</td><td>$${Number(datum.amount).toFixed(2)}</td></tr>`)
+        total += Number(datum.amount)
     })
 
     $table.append(`<tfoot class=""table-group-divider"><tr><th scope="row">Total</th><td></td><td>$${total.toFixed(2)}</td></tfoot>`)
--- routes/api/cashflow.cjs
+++ routes/api/cashflow.cjs
@@ -3,12 +3,22 @@ const Router = express.Router
 
 const cashflowApi = new Router()
 
-const User = require('../../classes/User.cjs')
+cashflowApi.post('/getTemp', (req, res) => {
+    let from = req.query.from
+    let to = req.query.to
+    console.log(from, ' to ', to)
+    let cashflow = req.user.budget.scheduleBetween(from, to)
+    console.log(cashflow)
+    res.send(cashflow)
+})
+
 
 cashflowApi.get('/', (req, res) => {
     console.log(User)
     let cashflowItems = user.cashflow.getItems()
     res.send(cashflowItems)
 })
+
+
 
 module.exports = cashflowApi
