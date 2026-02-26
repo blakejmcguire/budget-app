7a369452c226fae3e6f1561e39ff2417fdada6cb
Blake McGuire <blake@blakemcguire.com>
2026-02-26 10:10:59 -0800

    Changed nextPayment to always give next if input date is the same.

--- Budget.js
+++ Budget.js
@@ -217,7 +217,7 @@ class Budget {
         let fromIndex = Budget.dailyIndex(from, period)
         let difference = index - fromIndex
 
-        if (difference < 0) {
+        if (difference <= 0) {
             difference += period
         }
 
@@ -225,6 +225,16 @@ class Budget {
         output.setUTCDate(output.getUTCDate() + difference)
 
         return output
+    }
+
+    static scheduleBetween(startDate, endDate, index, paymentsPerYear) {
+        let schedule = []
+        let nextPayment = Budget.nextPayment(startDate, index, paymentsPerYear)
+        while (nextPayment <= endDate) {
+            schedule.push(nextPayment)
+            nextPayment = Budget.nextPayment(nextPayment, index, paymentsPerYear)
+        }
+        return schedule
     }
 }
 
